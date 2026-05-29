272d616340d8dda99837b4f8fdbcb4a88c6f5866
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-05-29 14:27:39 -0700

    configure user rights within clothingItem.js

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -42,9 +42,20 @@ const createClothingItem = (req, res) => {
 const deleteClothingItem = (req, res) => {
   const { itemId } = req.params;
 
-  ClothingItem.findByIdAndDelete(itemId)
+  ClothingItem.findById(itemId)
     .orFail()
-    .then((item) => res.status(200).send(item))
+    .then((item) => {
+      // ownership check
+      if (item.owner.toString() !== req.user._id) {
+        return res.status(403).send({
+          message: "You are not allowed to delete this item",
+        });
+      }
+
+      return item.deleteOne().then(() => {
+        res.status(200).send({ message: "Item deleted" });
+      });
+    })
     .catch((err) => {
       console.error(err);
 
