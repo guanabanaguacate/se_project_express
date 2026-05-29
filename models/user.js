aab71d0d4293689124784efd339faf0c7dc304c5
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-05-29 14:19:18 -0700

    set up fundUserCredentials in user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 2, maxlength: 30 },
-  
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+
   avatar: {
     type: String,
     required: true,
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
     validate: {
       validator(value) {
         return validator.isURL(value);
@@ -25,6 +20,37 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password") // override select:false
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Invalid email or password"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Invalid email or password"));
+        }
+
+        return user;
+      });
+    });
+};
+
 
 module.exports = mongoose.model("user", userSchema);
