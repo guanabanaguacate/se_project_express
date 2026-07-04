6eb2ea12ec221dd6a2fd7b4ac7f6b32450e96e41
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-07-03 21:00:24 -0700

    app.js updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -11,10 +12,14 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/", mainRouter);
+
+app.use(errors());
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
