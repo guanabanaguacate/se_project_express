e47decc051a9d0d655730569a4a8a7f7a7b3d586
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-07-03 21:05:45 -0700

    index and users routes updated with error handling

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,20 @@ const auth = require("../middlewares/auth");
 const userRouter = require("./users");
 const clothingItemRoutes = require("./clothingItem");
 
-router.post("/signup", createUser);
-router.post("/signin", login);
+const {
+  validateUserBody,
+  validateLogin,
+} = require("../middlewares/validation");
 
+// AUTH ROUTES
+router.post("/signup", validateUserBody, createUser);
+router.post("/signin", validateLogin, login);
+
+// PROTECTED ROUTES
 router.use("/users", auth, userRouter);
 router.use("/items", clothingItemRoutes);
 
+// 404
 router.use((req, res) => {
   res.status(NOT_FOUND).send({
     message: "Requested resource not found",
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const auth = require("../middlewares/auth");
+const {
+  getCurrentUser,
+  updateCurrentUser,
+} = require("../controllers/users");
 
-const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
+router.get("/me", getCurrentUser);
 
-router.get("/me", auth, getCurrentUser);
-router.patch("/me", auth, updateCurrentUser);
+router.patch("/me", updateCurrentUser);
 
 module.exports = router;
